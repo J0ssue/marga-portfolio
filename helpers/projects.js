108cb1e7999c02c462cbf1bdc6cb83b71a0312b9
Josue Diaz <jdiaz77711@gmail.com>
2020-06-01 15:39:16 +0100

    add remaining pictures to each project

--- helpers/projects.js
+++ helpers/projects.js
@@ -7,7 +7,17 @@ export default {
       subtitle: 'EDITORIAL DESIGN   183 PAGES, 109 MM X 196 MM',
       description:
         'contains eleven versions of one story, assembled with a collection of images from disparate online archives.Various modes of navigation are created through the association of images and story versions from different sources, with the hope that somewhere between them lies the germination of creativity.',
-      images: ['/images/projects/ns-narrative/pic-1.png']
+      images: [
+        '/images/projects/ns-narrative/1.png',
+        '/images/projects/ns-narrative/2.jpg',
+        '/images/projects/ns-narrative/3.jpg',
+        '/images/projects/ns-narrative/4.jpg',
+        '/images/projects/ns-narrative/5.jpg',
+        '/images/projects/ns-narrative/6.jpg',
+        '/images/projects/ns-narrative/7.jpg',
+        '/images/projects/ns-narrative/8.jpg',
+        '/images/projects/ns-narrative/9.jpg'
+      ]
     },
     {
       slug: 'line-editorial',
@@ -16,7 +26,18 @@ export default {
       subtitle: 'EDITORIAL DESIGN   168 PAGES, 182 MM X 135 MM',
       description:
         'is a critical reader of graphic design-led research that frames the act of publishing research through the metaphor of the roundabout; as in publishing, the roundabout allows circulation and instigates cooperation and interdependence; participants actively cooperate with each other, driving their research through parallel and intersecting lanes, developing in constant motion. ♥ <a class="hand-cursor" href="http://palomamoniz.com/" target="_blank">Paloma Moniz</a>, <a class="hand-cursor" href="http://roxyzeiher.com/" target="_blank">Roxy Zeiher</a>, <a class="hand-cursor" href="https://jaimedelcorro.com/" target="_blank">Jaime Del Carro</a>, <a class="hand-cursor" href="http://ricardo-goncalves.com/" target="_blank">Ricardo Gonçalves</a> and <a class="hand-cursor" href="https://www.instagram.com/rosa_que_nao_pica/" target="_blank">Francisca Roseiro</a>. Available at <a class="hand-cursor" href="https://shop.magculture.com/products/a-line-which-forms-a-volume-3?_pos=1&_sid=1d27693fe&_ss=r" target="_blank">MagCulture</a>',
-      images: ['/images/projects/line-editorial/pic-1.png']
+      images: [
+        '/images/projects/line-editorial/pic-1.png',
+        '/images/projects/line-editorial/2.JPG',
+        '/images/projects/line-editorial/3.jpeg',
+        '/images/projects/line-editorial/4.jpg',
+        '/images/projects/line-editorial/5.jpeg',
+        '/images/projects/line-editorial/6.JPG',
+        '/images/projects/line-editorial/7.jpeg',
+        '/images/projects/line-editorial/8.JPG',
+        '/images/projects/line-editorial/9.JPG',
+        '/images/projects/line-editorial/10.jpg'
+      ]
     },
     {
       slug: 'line-curation',
@@ -25,7 +46,18 @@ export default {
       subtitle: 'CURATION   LONDON COLLEGE OF COMMUNICATION',
       description:
         'is an event for active discourse on design-led research and practice. The third edition features contributions, talks and performances from <a class="hand-cursor" href="http://studiolegrandjager.com/projects">Legrand Jager</a>, <a class="hand-cursor" href="https://soulellis.com/">Paul Soullelis</a>, <a class="hand-cursor" href="https://evening-class.org/" target="_blank">Evening Class</a>, <a class="hand-cursor" href="https://www.andrewbrash.net/" target="_blank">Andrew Brash</a>, <a class="hand-cursor" href="http://www.anjakaiser.info/" target="_blank">Anja Kaiser</a> and <a class="hand-cursor" href="https://www.rebeccastephany.com/" target="_blank">Rebecca Stephany</a>,',
-      images: ['/images/projects/line-curation/pic-1.png']
+      images: [
+        '/images/projects/line-curation/1.png',
+        '/images/projects/line-curation/2.JPG',
+        '/images/projects/line-curation/3.jpeg',
+        '/images/projects/line-curation/4.jpg',
+        '/images/projects/line-curation/5.jpeg',
+        '/images/projects/line-curation/6.JPG',
+        '/images/projects/line-curation/7.jpeg',
+        '/images/projects/line-curation/8.JPG',
+        '/images/projects/line-curation/9.JPG',
+        '/images/projects/line-curation/10.jpg'
+      ]
     },
     {
       slug: 'modjo-design',
@@ -34,7 +66,17 @@ export default {
       subtitle: 'WEB DESIGN',
       description:
         'is a website for Modjo Design, a design agency in Lisbon collaborating with artists, designers and brands. Their concept of transparency was used for the website interactions, overlapping pages when opening menus and links.',
-      images: ['/images/projects/modjo/pic-1.png']
+      images: [
+        '/images/projects/modjo/1.jpg',
+        '/images/projects/modjo/2.jpg',
+        '/images/projects/modjo/3.jpg',
+        '/images/projects/modjo/4.jpg',
+        '/images/projects/modjo/5.jpg',
+        '/images/projects/modjo/6.jpg',
+        '/images/projects/modjo/7.jpg',
+        '/images/projects/modjo/8.jpg',
+        '/images/projects/modjo/9.jpg'
+      ]
     },
     {
       slug: 'picture-editorial',
@@ -43,7 +85,7 @@ export default {
       subtitle: 'EDITORIAL DESIGN   216 PAGES, 110 MM x 180 MM',
       description:
         'presents the exhibition’s intention through the review of the work displayed in several interviews.The catalogue questions our agency and position through a curatorial statement and is divided into two chapters that work as an exercise on associative image-research. ♥ <a class="hand-cursor" href="https://www.instagram.com/llichtblau/" target="_blank">Lisa Lichblau</a> and <a class="hand-cursor" href="https://rong-her21.tumblr.com/" target="_blank">Rong Lian.</a>',
-      images: ['/images/projects/pictures-editorial/pic-1.png']
+      images: ['/images/projects/pictures-editorial/1.png']
     },
     {
       slug: 'microscopic-editorial',
@@ -52,7 +94,18 @@ export default {
       subtitle: 'EDITORIAL DESIGN   56 PAGES, 210 MM x 297 MM',
       description:
         'uses visual similarity as a tool to navigate information. The publication is based on narratives of association which creates an assemblage of information based on resemblance, that disrupts categorizatio and ranges freely across different scales of information.',
-      images: ['/images/projects/microscopic-editorial/pic-1.png']
+      images: [
+        '/images/projects/microscopic-editorial/1.png',
+        '/images/projects/microscopic-editorial/2.jpg',
+        '/images/projects/microscopic-editorial/3.jpg',
+        '/images/projects/microscopic-editorial/4.jpg',
+        '/images/projects/microscopic-editorial/5.jpg',
+        '/images/projects/microscopic-editorial/6.jpg',
+        '/images/projects/microscopic-editorial/7.jpg',
+        '/images/projects/microscopic-editorial/8.jpg',
+        '/images/projects/microscopic-editorial/9.jpg',
+        '/images/projects/microscopic-editorial/10.jpg'
+      ]
     },
     {
       slug: 'algorithm',
@@ -61,7 +114,19 @@ export default {
       subtitle: 'EDITORIAL DESIGN   40 PAGES, 297 MM x 420 MM',
       description:
         'is a booklet on archive’s navigation. Methods for organization of archives online are appropriated using algorithmic tools to make connections between images building various visual narratives.',
-      images: ['/images/projects/algorithmic-editorial/pic-1.png']
+      images: [
+        '/images/projects/algorithmic-editorial/1.png',
+        '/images/projects/algorithmic-editorial/2.jpg',
+        '/images/projects/algorithmic-editorial/3.jpeg',
+        '/images/projects/algorithmic-editorial/4.jpeg',
+        '/images/projects/algorithmic-editorial/5.jpeg',
+        '/images/projects/algorithmic-editorial/6.jpg',
+        '/images/projects/algorithmic-editorial/7.jpg',
+        '/images/projects/algorithmic-editorial/8.jpeg',
+        '/images/projects/algorithmic-editorial/9.jpg',
+        '/images/projects/algorithmic-editorial/10.jpeg',
+        '/images/projects/algorithmic-editorial/11.jpeg'
+      ]
     },
     {
       slug: 'molecular',
@@ -69,7 +134,12 @@ export default {
       subtitle: 'VISUAL IDENTITY',
       description:
         'Is an album from an electronic music duo from Porto. The graphic identity is inspired in their music, which relates to molecular and spatial imagery.',
-      images: ['/images/projects/molecular/pic-1.png']
+      images: [
+        '/images/projects/molecular/1.png',
+        '/images/projects/molecular/2.png',
+        '/images/projects/molecular/3.png',
+        '/images/projects/molecular/4.png'
+      ]
     }
   ]
 }
